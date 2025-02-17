c17da2d9d85bbfafe27ab29bd36a87f2bb55ac1a
dorichapi <38826817+dorichapi@users.noreply.github.com>
2025-02-17 17:45:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const apiUrl = "https://script.google.com/macros/s/AKfycbzFNOekouxWlJ3g_q6Fg3ZXTX8udctKQSBKAwkupswvDaT5GJAF2dc2t1mDMdT2jA9q/exec";
 
 // ✅ Google Apps ScriptのURL（スプレッドシート1のデータ取得用）
-const apiUrl1 = "https://docs.google.com/spreadsheets/d/1Ka4nZ1hoKhPIbZf8IdAuErfhEfGMnyi6EzEYMalJNkM/edit?gid=0#gid=0";
-const apiUrl2 = "https://docs.google.com/spreadsheets/d/1ONAQXCxwSMUjyoUAZ6Gg5JNu_jEGPu92l7L01RSSyko/edit?gid=0#gid=0";
+const apiUrl1 = "https://script.google.com/macros/s/AKfycbyHjaYy9fe53yVhRs8ZIgFkIvU_CnK4jxvedThJdUnUqnjlnjB_ibQTWHsoeIt7MBwijQ/exec";
+const apiUrl2 = "https://script.google.com/macros/s/AKfycbwBNbVT7l_fQIplHVwyY8KaMUkbqmUGag2WHww9BMWiZBtWk12ZHAESxYkvzulopU2x/exec";
 
 // ✅ データ取得 & グラフ表示
 async function fetchData() {
